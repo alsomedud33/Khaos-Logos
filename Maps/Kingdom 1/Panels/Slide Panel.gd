cfbbe2f84d49ee612a5ad9997ccbfcccde611554
alsomedud33 <Alsomedude@hotmail.com>
2023-06-11 20:04:08 +0100

    Added Videos to Tutorial

--- Maps/Kingdom 1/Panels/Slide Panel.gd
+++ Maps/Kingdom 1/Panels/Slide Panel.gd
@@ -1,9 +1,11 @@
 extends MeshInstance3D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	if self.global_position.distance_to(%RocketMan.global_position) > 15:
+		$SubViewport/Control/VideoStreamPlayer.set_paused(true)
 		$SubViewport/Control/VideoStreamPlayer.stop()
 	else:
 		if $SubViewport/Control/VideoStreamPlayer.is_playing() == false:
+			$SubViewport/Control/VideoStreamPlayer.set_paused(false)
 			$SubViewport/Control/VideoStreamPlayer.play()
